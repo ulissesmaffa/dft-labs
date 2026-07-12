d7cafc7af7ea35816f3d8228af208f31b651d599
Ulisses Maffazioli <ulissesmaffa@gmail.com>
2026-07-12 16:48:19 -0300

    [draft] commit with outputs

---
+++ proj/santiago_cpu/0_synopsys_saed32_flow/_dft/2_advisor_best_practice/sg_results/santiago/top_santiago_cpu/dft/dft_best_practice/spyglass_spysch/sg_del.pl
@@ -0,0 +1,5 @@
+use spyglass;
+use SpyGlass;
+use SpyGlass::Objects;
+&spyGenerateDelViolHash("./sg_results/santiago/top_santiago_cpu/dft/dft_best_practice/spyglass_spysch/sg_msgtag.txt");
+1;
---
+++ proj/santiago_cpu/0_synopsys_saed32_flow/_dft/2_advisor_best_practice/sg_results/santiago/top_santiago_cpu/dft/dft_best_practice/spyglass_spysch/sg_msgtag.pl
@@ -0,0 +1,31 @@
+################################################################################
+#This is an internally genertaed by SpyGlass for Message Tagging Support
+################################################################################
+
+
+use spyglass;
+use SpyGlass;
+use SpyGlass::Objects;
+spyRebootMsgTagSupport();
+
+spySetMsgTagCount(363,63);
+spyCacheTagValuesFromBatch(["Async_02_capture_CSV_TAG"]);
+spyCacheTagValuesFromBatch(["Async_07_CSV_TAG"]);
+spyCacheTagValuesFromBatch(["Async_08_CSV_TAG"]);
+spyCacheTagValuesFromBatch(["DFT_DATA_CSV_TAG"]);
+spyCacheTagValuesFromBatch(["Diagnose_testclock_CSV_TAG"]);
+spyCacheTagValuesFromBatch(["pe_crossprobe_tag"]);
+spyParseTextMessageTagFile("./sg_results/santiago/top_santiago_cpu/dft/dft_best_practice/spyglass_spysch/sg_msgtag.txt");
+
+if(!defined $::spyInIspy || !$::spyInIspy)
+{
+    spyDefineReportGroupingOrder("ALL",
+(
+"BUILTIN"   => [SGTAGTRUE, SGTAGFALSE]
+,"TEMPLATE" => "A"
+)
+);
+}
+spyMessageTagTestBenchmark(64,"./sg_results/santiago/top_santiago_cpu/dft/dft_best_practice/spyglass.vdb");
+
+1;
